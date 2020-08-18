7ba9cef1e46f1798f2643c57688e40d94a26ac23
Swani Eryani <aleryanisilwan@gmail.com>
2020-08-18 20:59:07 +0000

    assertArraysEqual created

---
+++ lotide/assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  let returnVal = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        returnVal = true;
+      } else {
+        returnVal = false;
+        break;
+      }
+    }
+  } else {
+    return false;
+  }
+  return returnVal;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`✖️ ✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✔️ ✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
