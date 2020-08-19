7b12ad96fc667b58efe1e62f81018e9e79f590ab
Swani Eryani <aleryanisilwan@gmail.com>
2020-08-19 20:06:02 +0000

    eqObjects had added

--- lotide/eqObjects.js
+++ lotide/eqObjects.js
@@ -8,13 +8,36 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1, arr2) {
+  let returnVal = true;
+  if (arr1 && arr2) {
+    if (arr1.length === arr2.length) {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i]) {
+          returnVal = true;
+        } else {
+          returnVal = false;
+          break;
+        }
+      }
+    } else {
+      return false;
+    }
+    return returnVal;
+  }
+  return false;
+};
+
+
 const eqObjects = function(object1, object2) {
   let returnVal;
   let keysObj1 = Object.keys(object1);
   let keysObj2 = Object.keys(object2);
   if (keysObj1.length === keysObj2.length) {
     for (const obj in object1) {
-      if (object2[obj] === object1[obj]) {
+      if (Array.isArray(object2[obj]) && Array.isArray(object1[obj])) {
+        return eqArrays(object2[obj], object1[obj]);
+      } else if (object2[obj] === object1[obj]) {
         returnVal = true;
       } else {
         return false;
@@ -37,3 +60,11 @@ const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
 
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+//eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
