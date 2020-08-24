a86bd6dd54473e92afb1a9a530dca2c01fe45836
Swani Eryani <aleryanisilwan@gmail.com>
2020-08-24 17:31:56 +0000

    middle refactored

--- lotide/middle.js
+++ lotide/middle.js
@@ -1,26 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let returnVal = true;
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        returnVal = true;
-      } else {
-        returnVal = false;
-        break;
-      }
-    }
-  } else {
-    return false;
-  }
-  return returnVal;
-};
-const assertArraysEqual = function(actual, expected) {
-  if (!eqArrays(actual, expected)) {
-    console.log("\x1b[31m%s\x1b[0m",`✖️ ✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log("\x1b[32m%s\x1b[0m",`✔️ ✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+
 
 const middle = function(array) {
   
@@ -45,14 +23,6 @@ const middle = function(array) {
 };
 
 
-//test length < 2
-assertArraysEqual(middle([1]), []);// => []
-assertArraysEqual(middle([1, 2]), []); // => []
 
-//test length odd
-assertArraysEqual(middle([1, 2, 3]), [2]);// => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);// => [3]
 
-// test length even
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);// => [3, 4]
+module.exports = middle;
