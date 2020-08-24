6bd6cb50c8b73288f0486a66839cf16c10bf1ba3
Swani Eryani <aleryanisilwan@gmail.com>
2020-08-24 17:06:23 +0000

    assertEqual refactored

--- lotide/assertEqual.js
+++ lotide/assertEqual.js
@@ -1,12 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log(" ✖️ ✖️ ✖️ ✖️  Assertion Failed: " + actual + " !== " + expected);
-  }
-  else {
-    console.log(" ✔️ ✔️ ✔️ ✔️  Assertion Passed: " + actual + " === " + expected);
+    //console.log(" ✖️ ✖️ ✖️ ✖️  Assertion Failed: " + actual + " !== " + expected);
+    console.log("\x1b[31m%s\x1b[0m",`✖️ ✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    //console.log(" ✔️ ✔️ ✔️ ✔️  Assertion Passed: " + actual + " === " + expected);
+    console.log("\x1b[32m%s\x1b[0m",`✔️ ✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");//Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
-assertEqual(1, 1);
+module.exports = assertEqual;
