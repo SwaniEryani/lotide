05a7699559e3dd739178de132b7514fd54f5bff5
Swani Eryani <aleryanisilwan@gmail.com>
2020-08-24 18:02:44 +0000

    middle refactored (M & C)

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,4 +1,14 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, ["Lighthouse", "Labs"].length);
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+  it("returns undefined for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+});
